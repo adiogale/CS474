34b367de98d7b511fc30da425a075bffe178f971
adiogale <adityaogale1996@gmail.com>
2022-02-11 16:42:25 -0600

    Added implementation of Scope

--- src/main/scala/SetOperations.scala
+++ src/main/scala/SetOperations.scala
@@ -1,15 +1,20 @@
 import scala.collection.mutable
 import scala.collection.mutable.Set
 import scala.collection.mutable.Map
+import scala.collection.mutable.ListBuffer
 
 
 object SetOperations {
   private val bindings: mutable.Map[String, Any] = mutable.Map[String, Any]("x" -> mutable.Set(3, 4))
   private val macroMap: mutable.Map[String, SetOperations] = mutable.Map[String, SetOperations]()
+  private val scopeBindings: mutable.Map[String, mutable.Map[String, Any]] = mutable.Map[String, mutable.Map[String, Any]]("global" -> bindings)
+  // Default scope is "global".
+  private val curScope: ListBuffer[String] = ListBuffer[String]("global")
 
   enum SetOperations:
     case ValueOf(varName: Any)
     case Variable(varName: String)
+    case Set(varName: mutable.Set[Any])
     case Assign(varName: String, element: SetOperations)
     case Insert(varName: String, element: SetOperations)
     case CreateSet(elements: Any*)
@@ -22,32 +27,38 @@ object SetOperations {
     case CheckElement(setName: SetOperations, element: SetOperations)
     case SetMacro(macroName: String, macroValue: SetOperations)
     case GetMacro(macroName:String)
+    case Scope(scopeName: String, varName: String, element: SetOperations)
+    case getSetFromScope(scopeName: String, setName: String)
 
     def eval: Any =
       this match
         case ValueOf(varName) => varName
 
         case Variable(varName) =>
-          if (bindings.contains(varName))
-            bindings(varName.asInstanceOf[String])
+          if (scopeBindings(curScope.head).contains(varName))
+            scopeBindings(curScope.head)(varName.asInstanceOf[String])
           else 0
 
         case Assign(varName, element) =>
-          bindings += (varName -> element.eval)
-          bindings(varName)
+          scopeBindings(curScope.head) += (varName -> element.eval)
+          scopeBindings(curScope.head)(varName)
 
         case CreateSet(elements*) =>
           val new_bindings = mutable.Set[Any](elements *)
           new_bindings
 
         case Insert(varName, element) =>
-          if (bindings.contains(varName))
-            val new_bindings = bindings(varName).asInstanceOf[mutable.Set[Any]] + element.eval.asInstanceOf[Any]
-            bindings += (varName -> new_bindings)
+          // If binding is available for varName, add element to the set
+          // Using curScope.head to get in whcich scope operation is being performed into.
+          // See more in Scope.
+          if (scopeBindings(curScope.head).contains(varName))
+            val new_bindings = scopeBindings(curScope.head)(varName).asInstanceOf[mutable.Set[Any]] + element.eval.asInstanceOf[Any]
+            scopeBindings(curScope.head) += (varName -> new_bindings)
             new_bindings
+          //Else create a new set and add element to the set
           else
             val new_bindings = mutable.Set(element.eval)
-            bindings += (varName -> new_bindings)
+            scopeBindings(curScope.head) += (varName -> new_bindings)
             new_bindings
 
         case Union(setName1, setName2) =>
@@ -60,10 +71,12 @@ object SetOperations {
           setName1.eval.asInstanceOf[mutable.Set[Any]].diff(setName2.eval.asInstanceOf[mutable.Set[Any]])
 
         case Symmetric_difference(setName1, setName2) =>
+          // = (A-B)U(B-A)
           (setName1.eval.asInstanceOf[mutable.Set[Any]].diff(setName2.eval.asInstanceOf[mutable.Set[Any]])).
             union(setName2.eval.asInstanceOf[mutable.Set[Any]].diff(setName1.eval.asInstanceOf[mutable.Set[Any]]))
 
         case Product(setName1, setName2) =>
+          //Creates a set of tuples of all possible combinations of the sets
           val new_binding = mutable.Set[Any]()
           for (i <- setName1.eval.asInstanceOf[mutable.Set[Any]])
             for (j <- setName2.eval.asInstanceOf[mutable.Set[Any]])
@@ -71,14 +84,17 @@ object SetOperations {
           new_binding
 
         case Delete(varName, element) =>
-          bindings(varName).asInstanceOf[mutable.Set[Any]] -= element.eval.asInstanceOf[Any]
+          scopeBindings(curScope.head)(varName).asInstanceOf[mutable.Set[Any]] -= element.eval.asInstanceOf[Any]
+          scopeBindings(curScope.head)(varName)
 
         case CheckElement(varName, element) =>
+          // Returns True if element present in varName. Else False.
           val tempVal = varName.eval
           if (tempVal.isInstanceOf[mutable.Set[Any]])
             if (tempVal.asInstanceOf[mutable.Set[Any]].contains(element.eval))
               true
             else false
+          // If Variable name not present, returns "Variable Name is not a set"
           else
             "Variable Name is not a set"
 
@@ -88,13 +104,30 @@ object SetOperations {
         case GetMacro(macroName) =>
           macroMap(macroName).eval
 
-  @main def main(): Unit =
-    import SetOperations.*
-    Assign("x", CreateSet(1,2)).eval
-    var varName = 9
-    SetMacro("insert", Insert("x", ValueOf(varName))).eval
-    varName = 10
-    Assign("r", GetMacro("insert")).eval
-    print(Variable("r").eval)
-//    Variable("r").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2,10)
+        case getSetFromScope(scopeName, setName) =>
+          //Returns set from given scope.
+          scopeBindings(scopeName)(setName)
+
+        case Scope(scopeName, varName, element) =>
+          // If scope is not yet present, create one.
+          if (!scopeBindings.contains(scopeName)){
+            scopeBindings += (scopeName -> mutable.Map[String, Any]())
+          }
+          // Default scope is "global", change it to given scopeName.
+          curScope.remove(0)
+          curScope += scopeName
+
+          // Perform the action in the given scopeName. If in scopeName, varNAme is present, add to it.
+          if (scopeBindings(scopeName).contains(varName)) {
+            val new_binding = scopeBindings(scopeName)(varName).asInstanceOf[mutable.Set[Any]].union(element.eval.asInstanceOf[mutable.Set[Any]])
+            scopeBindings(scopeName)(varName) = new_binding
+          }
+          //Else create new one.
+          else {
+            scopeBindings(scopeName) += (varName -> element.eval.asInstanceOf[Any])
+            scopeBindings(scopeName)(varName).asInstanceOf[mutable.Set[Any]]
+          }
+          //Restore to default global scope.
+          curScope.remove(0)
+          curScope += "global"
 }
--- src/test/scala/SetOperationsTest.scala
+++ src/test/scala/SetOperationsTest.scala
@@ -24,6 +24,16 @@ class SetOperationsTest extends AnyFlatSpec with Matchers {
     Variable("z").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (4,3)
   }
 
+  behavior of "union"
+  it should "show all elements in both sets" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Insert("x", ValueOf(4)).eval
+    Assign("y", CreateSet(3,4)).eval
+    Insert("y", Variable("x")).eval
+    Assign("z", Union(Variable("x"), Variable("y"))).eval
+    Variable("z").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2, 4,3)
+  }
+
   behavior of "difference"
   it should "show element in first set that are not present in second set" in {
     Assign("x", CreateSet(1,2,3)).eval
@@ -32,6 +42,14 @@ class SetOperationsTest extends AnyFlatSpec with Matchers {
     Insert("y", ValueOf(5)).eval
     Assign("p", Difference(Variable("x"), Variable("y"))).eval
     Variable("p").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2)
+  }
+
+  behavior of "Cartesian product"
+  it should "show set of tuples of all possible combinations of set1 and set2" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Assign("y", CreateSet(4,5)).eval
+    Assign("p", Product(Variable("x"), Variable("y"))).eval
+    Variable("p").eval.asInstanceOf[mutable.Set[Any]] should contain allOf ((1,4),(1,5),(2,4),(2,5),(3,4),(3,5))
   }
 
   behavior of "symmetric difference"
@@ -61,4 +79,24 @@ class SetOperationsTest extends AnyFlatSpec with Matchers {
     Assign("r", GetMacro("insert")).eval
     Variable("r").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2,9)
   }
+
+  behavior of "scope"
+  it should "match given sets" in {
+    Assign("x", CreateSet(1,2)).eval
+    getSetFromScope("global", "x").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2)
+    Assign("y", CreateSet(3,4)).eval
+    getSetFromScope("global", "y").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (3,4)
+    Scope("scope1", "x", CreateSet(5,6)).eval
+    getSetFromScope("scope1", "x").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (5,6)
+    Scope("scope2", "x", Insert("x",ValueOf(6))).eval
+    getSetFromScope("scope2", "x").eval.asInstanceOf[mutable.Set[Any]] should contain (6)
+    Scope("scope2", "x", Insert("x",ValueOf(5))).eval
+    getSetFromScope("scope2", "x").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (5,6)
+    Scope("scope2", "x", Insert("x",ValueOf(7))).eval
+    getSetFromScope("scope2", "x").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (5,6,7)
+    Scope("scope1", "x", Delete("x", ValueOf(5))).eval
+    getSetFromScope("scope1", "x").eval.asInstanceOf[mutable.Set[Any]] should contain (6)
+    getSetFromScope("scope2", "x").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (6,5,7)
+  }
+
 }
