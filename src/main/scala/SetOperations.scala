07cbfe6e4b8c53a9e5af5e8f6cde86763072b800
adiogale <adityaogale1996@gmail.com>
2022-02-11 03:52:50 -0600

    First commit

---
+++ src/main/scala/SetOperations.scala
@@ -0,0 +1,99 @@
+import scala.collection.mutable
+import scala.collection.mutable.Set
+import scala.collection.mutable.Map
+
+
+object SetOperations {
+  private val bindings: mutable.Map[String, Any] = mutable.Map[String, Any]("x" -> mutable.Set(3, 4))
+  private val macroMap: mutable.Map[String, SetOperations] = mutable.Map[String, SetOperations]()
+
+  enum SetOperations:
+    case ValueOf(varName: Any)
+    case Variable(varName: String)
+    case Assign(varName: String, element: SetOperations)
+    case Insert(varName: String, element: SetOperations)
+    case CreateSet(elements: Any*)
+    case Delete(varName: String, element: SetOperations)
+    case Union(setName1: SetOperations, setName2: SetOperations)
+    case Intersection(setName1: SetOperations, setName2: SetOperations)
+    case Difference(setName1: SetOperations, setName2: SetOperations)
+    case Symmetric_difference(setName1: SetOperations, setName2: SetOperations)
+    case Product(setName1: SetOperations, setName2: SetOperations)
+    case CheckElement(setName: SetOperations, element: SetOperations)
+    case Macro(macroName: String, macroValue: SetOperations)
+//    case Macro(macroName:String)
+
+    def eval: Any =
+      this match
+        case ValueOf(varName) => varName
+
+        case Variable(varName) =>
+          if (bindings.contains(varName))
+            bindings(varName.asInstanceOf[String])
+          else 0
+
+        case Assign(varName, element) =>
+          bindings += (varName -> element.eval)
+          bindings(varName)
+
+        case CreateSet(elements*) =>
+          val new_bindings = mutable.Set[Any](elements *)
+          new_bindings
+
+        case Insert(varName, element) =>
+          if (bindings.contains(varName))
+            val new_bindings = bindings(varName).asInstanceOf[mutable.Set[Any]] + element.eval.asInstanceOf[Any]
+            bindings += (varName -> new_bindings)
+            new_bindings
+          else
+            val new_bindings = mutable.Set(element.eval)
+            bindings += (varName -> new_bindings)
+            new_bindings
+
+        case Union(setName1, setName2) =>
+          setName1.eval.asInstanceOf[mutable.Set[Any]].union(setName2.eval.asInstanceOf[mutable.Set[Any]])
+
+        case Intersection(setName1, setName2) =>
+          setName1.eval.asInstanceOf[mutable.Set[Any]].intersect(setName2.eval.asInstanceOf[mutable.Set[Any]])
+
+        case Difference(setName1, setName2) =>
+          setName1.eval.asInstanceOf[mutable.Set[Any]].diff(setName2.eval.asInstanceOf[mutable.Set[Any]])
+
+        case Symmetric_difference(setName1, setName2) =>
+          (setName1.eval.asInstanceOf[mutable.Set[Any]].diff(setName2.eval.asInstanceOf[mutable.Set[Any]])).
+            union(setName2.eval.asInstanceOf[mutable.Set[Any]].diff(setName1.eval.asInstanceOf[mutable.Set[Any]]))
+
+        case Product(setName1, setName2) =>
+          val new_binding = mutable.Set[Any]()
+          for (i <- setName1.eval.asInstanceOf[mutable.Set[Any]])
+            for (j <- setName2.eval.asInstanceOf[mutable.Set[Any]])
+              new_binding += ((i, j))
+          new_binding
+
+        case Delete(varName, element) =>
+          bindings(varName).asInstanceOf[mutable.Set[Any]] -= element.eval.asInstanceOf[Any]
+
+        case CheckElement(varName, element) =>
+          val tempVal = varName.eval
+          if (tempVal.isInstanceOf[mutable.Set[Any]])
+            if (tempVal.asInstanceOf[mutable.Set[Any]].contains(element.eval))
+              true
+            else false
+          else
+            "Variable Name is not a set"
+
+        case Macro(macroName, macroValue) =>
+          macroMap += (macroName -> macroValue)
+
+//        case Macro(macroName) =>
+//          macroMap(macroName)
+
+  @main def main(): Unit =
+    import SetOperations.*
+    Assign("x", CreateSet(1,2,3)).eval
+    Insert("x", ValueOf(4)).eval
+    Assign("y", CreateSet(3,4)).eval
+    Insert("y", Variable("x")).eval
+    println("y: " + Variable("y").eval + "\n")
+    println("x: " + Variable("x").eval)
+}
---
+++ src/test/scala/SetOperationsTest.scala
@@ -0,0 +1,55 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.must.Matchers.*
+import SetOperations.SetOperations.*
+import org.scalatest.enablers.Containing
+
+import scala.collection.mutable
+
+class SetOperationsTest extends AnyFlatSpec with Matchers {
+
+  behavior of "create a set"
+  it should "add element" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Variable("x").eval.asInstanceOf[mutable.Set[Any]]  should contain allOf (1,2,3)
+  }
+
+  behavior of "intersection"
+  it should "show only common elements to both sets" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Insert("x", ValueOf(4)).eval
+    Assign("y", CreateSet(3,4)).eval
+    Insert("y", Variable("x")).eval
+    Assign("z", Intersection(Variable("x"), Variable("y"))).eval
+    Variable("z").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (4,3)
+  }
+
+  behavior of "difference"
+  it should "show element in first set that are not present in second set" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Insert("x", ValueOf(4)).eval
+    Assign("y", CreateSet(3,4)).eval
+    Insert("y", ValueOf(5)).eval
+    Assign("p", Difference(Variable("x"), Variable("y"))).eval
+    Variable("p").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2)
+  }
+
+  behavior of "symmetric difference"
+  it should "show element not common to both of the sets" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Insert("x", ValueOf(4)).eval
+    Assign("y", CreateSet(3,4)).eval
+    Insert("y", ValueOf(5)).eval
+    Assign("p", Symmetric_difference(Variable("x"), Variable("y"))).eval
+    Variable("p").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (1,2,5)
+  }
+
+  behavior of "delete"
+  it should "Element is deleted from given set" in {
+    Assign("x", CreateSet(1,2,3)).eval
+    Insert("x", ValueOf(4)).eval
+    Delete("x", ValueOf(1)).eval
+    Variable("x").eval.asInstanceOf[mutable.Set[Any]] should contain allOf (3,2,4)
+  }
+
+}
